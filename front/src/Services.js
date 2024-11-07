8f4894c1234b6f52d09eaa73b306506c861042bc
Zartan229 <maxime.ortuno@hotmail.fr>
2024-11-07 19:01:30 +0100

    bon fichier

--- front/src/Services.js
+++ front/src/Services.js
@@ -1,31 +1,45 @@
 //Par défaut passe par le back-end, si le back-end pas lancer, envoyer sur les Mocks
 const { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } = require('./Mock.js');
 
-class UserService {
-    constructor() {
-        this.USER_MAIN_DATA = USER_MAIN_DATA;
-        this.USER_ACTIVITY = USER_ACTIVITY;
-        this.USER_AVERAGE_SESSIONS = USER_AVERAGE_SESSIONS;
-        this.USER_PERFORMANCE = USER_PERFORMANCE;
-    }
-
-    getUserData(userId) {
-        return this.USER_MAIN_DATA.find(user => user.id === userId);
-    }
-
-    getUserActivity(userId) {
-        return this.USER_ACTIVITY.find(activity => activity.userId === userId);
-    }
-
-    getUserAverageSessions(userId) {
-        return this.USER_AVERAGE_SESSIONS.find(session => session.userId === userId);
-    }
-
-    getUserPerformance(userId) {
-        return this.USER_PERFORMANCE.find(performance => performance.userId === userId);
-    }
+class User {
+  constructor({ id, userInfos, score, keyData }) {
+    this.id = id;
+    this.userInfos = userInfos;
+    this.score = score;
+    this.keyData = keyData;
+  }
 }
 
-const userService = new UserService();
+class Activity {
+  constructor({ userId, sessions }) {
+    this.userId = userId;
+    this.sessions = sessions;
+  }
+}
 
-module.exports = userService;
+class AverageSession {
+  constructor({ userId, sessions }) {
+    this.userId = userId;
+    this.sessions = sessions;
+  }
+}
+
+class Performance {
+  constructor({ userId, kind, data }) {
+    this.userId = userId;
+    this.kind = kind;
+    this.data = data;
+  }
+}
+
+const userInstances = USER_MAIN_DATA.map(user => new User(user));
+const activityInstances = USER_ACTIVITY.map(activity => new Activity(activity));
+const averageSessionInstances = USER_AVERAGE_SESSIONS.map(averageSession => new AverageSession(averageSession));
+const performanceInstances = USER_PERFORMANCE.map(performance => new Performance(performance));
+
+module.exports = {
+  userInstances,
+  activityInstances,
+  averageSessionInstances,
+  performanceInstances
+};
