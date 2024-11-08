5714332d1f6a8a983248c7bcfdcb89a3362ac555
Zartan229 <maxime.ortuno@hotmail.fr>
2024-11-08 13:36:51 +0100

    model

--- front/src/Model.js
+++ front/src/Model.js
@@ -1,6 +1,3 @@
-//Par défaut passe par le back-end, si le back-end pas lancer, envoyer sur les Mocks
-const { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } = require('./Mock.js');
-
 class User {
   constructor({ id, userInfos, score, keyData }) {
     this.id = id;
@@ -32,15 +29,10 @@ class Performance {
   }
 }
 
-const userInstances = USER_MAIN_DATA.map(user => new User(user));
-const activityInstances = USER_ACTIVITY.map(activity => new Activity(activity));
-const averageSessionInstances = USER_AVERAGE_SESSIONS.map(averageSession => new AverageSession(averageSession));
-const performanceInstances = USER_PERFORMANCE.map(performance => new Performance(performance));
-
 module.exports = {
-  userInstances,
-  activityInstances,
-  averageSessionInstances,
-  performanceInstances
+  User,
+  Activity,
+  AverageSession,
+  Performance
 };
 
