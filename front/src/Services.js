032b7a9a39d1eac47e4e0aa72e686955d851b2fe
Zartan229 <maxime.ortuno@hotmail.fr>
2024-11-08 13:31:23 +0100

    services, manque le if not work mock

--- front/src/Services.js
+++ front/src/Services.js
@@ -1,7 +1,9 @@
+import { User, Activity, AverageSession, Performance } from './Model.js';
+
 const API_BASE_URL = 'http://localhost:3000';
 
 
-function fetchUser(userId) {
+export function fetchUser(userId) {
     return fetch(`${API_BASE_URL}/user/${userId}`)
         .then(response => {
             if (!response.ok) {
@@ -9,17 +11,15 @@ function fetchUser(userId) {
             }
             return response.json();
         })
-        .then(data => {
-            console.log('User Data:', data); 
-            return data;
+        .then(responseData => {
+            return new User(responseData.data);  
         })
         .catch(error => {
             console.error('Error fetching user:', error);
         });
 }
 
-
-function fetchUserActivity(userId) {
+export function fetchUserActivity(userId) {
     return fetch(`${API_BASE_URL}/user/${userId}/activity`)
         .then(response => {
             if (!response.ok) {
@@ -27,16 +27,15 @@ function fetchUserActivity(userId) {
             }
             return response.json();
         })
-        .then(data => {
-            console.log('User Activity:', data); 
-            return data;
+        .then(responseData => {
+            return new Activity(responseData.data);
         })
         .catch(error => {
             console.error('Error fetching user activity:', error);
         });
 }
 
-function fetchUserAverageSessions(userId) {
+export function fetchUserAverageSessions(userId) {
     return fetch(`${API_BASE_URL}/user/${userId}/average-sessions`)
         .then(response => {
             if (!response.ok) {
@@ -44,17 +43,15 @@ function fetchUserAverageSessions(userId) {
             }
             return response.json();
         })
-        .then(data => {
-            console.log('User Average Sessions:', data);  
-            return data;
+        .then(responseData => {
+            return new AverageSession(responseData.data);
         })
         .catch(error => {
             console.error('Error fetching user average sessions:', error);
         });
 }
 
-
-function fetchUserPerformance(userId) {
+export function fetchUserPerformance(userId) {
     return fetch(`${API_BASE_URL}/user/${userId}/performance`)
         .then(response => {
             if (!response.ok) {
@@ -62,18 +59,10 @@ function fetchUserPerformance(userId) {
             }
             return response.json();
         })
-        .then(data => {
-            console.log('User Performance:', data);  
-            return data;
+        .then(responseData => {
+            return new Performance(responseData.data);
         })
         .catch(error => {
             console.error('Error fetching user performance:', error);
         });
 }
-
-const userId = 12;
-
-fetchUser(userId);
-fetchUserActivity(userId);
-fetchUserAverageSessions(userId); 
-fetchUserPerformance(userId);
